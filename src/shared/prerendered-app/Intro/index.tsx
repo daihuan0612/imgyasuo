c9f5e642274c295d09c37a2015db0fa578e50460
daihuan0612 <daihuan0612@gmail.com>
2026-08-08 14:07:41 +0800

    底部版权署名改为中文名：© 2026 by 空行

--- src/shared/prerendered-app/Intro/index.tsx
+++ src/shared/prerendered-app/Intro/index.tsx
@@ -402,7 +402,7 @@ export default class Intro extends Component<Props, State> {
         <footer class={style.footer}>
           <div class={style.footerContainer}>
             <div class={style.footerPadding}>
-              <div class={style.footerCredit}>© 2026 by kongxing</div>
+              <div class={style.footerCredit}>© 2026 by 空行</div>
             </div>
           </div>
         </footer>
